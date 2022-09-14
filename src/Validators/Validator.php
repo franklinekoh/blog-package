b17a64561432f9ebf011b4b1027aeb79a856ead8
Ekoh <Ekoh Franklin>
2022-09-14 14:31:32 +0100

    introduction to validation module

--- src/Validators/Validator.php
+++ src/Validators/Validator.php
@@ -1,6 +1,25 @@
 <?php
 
-class Validator
+namespace Practice\BlogPost\Validators;
+
+class Validator implements ValidatorContract
 {
 
+    protected array $request = [];
+    protected array $errors = [];
+
+    public function errors(): array
+    {
+        $this->errors();
+    }
+
+    public function validate(): array
+    {
+
+    }
+
+    public function rules(): array
+    {
+        return [];
+    }
 }
--- src/Validators/ValidatorContract.php
+++ src/Validators/ValidatorContract.php
@@ -2,7 +2,27 @@
 
 namespace Practice\BlogPost\Validators;
 
-class ValidatorContract
+interface ValidatorContract
 {
 
+    /**
+     * Returns validation errors
+     *
+     * @return array
+     */
+    public function errors(): array;
+
+    /**
+     * Validates user input
+     *
+     * @return array
+     */
+    public function validate(): array;
+
+    /**
+     * Defines rules for validation
+     *
+     * @return array
+     */
+    public function rules(): array;
 }
